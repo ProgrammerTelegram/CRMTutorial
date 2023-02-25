3cff215bee5a0289b0e236632da6a96498f77f7e
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-24 22:59:52 -0600

    added ContactForm

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,4 +1,45 @@
 package com.example.application.views.list;
 
-public class ContactForm {
+import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.List;
+
+
+public class ContactForm extends FormLayout {
+    TextField firstName = new TextField("First Name");
+    TextField lastName = new TextField("Last Name");
+    EmailField email = new EmailField("Email");
+    ComboBox<Status> status = new ComboBox<Status>("Status");
+    ComboBox<Company> company = new ComboBox<Company>("Company");
+
+    Button save = new Button("Save");
+    Button delete = new Button("Delete");
+    Button close = new Button("Cancel");
+
+    public ContactForm(List<Company> companies, List<Status> statuses) {
+        addClassName("contact-form");
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+        status.setItems(statuses);
+        status.setItemLabelGenerator(Status::getName);
+        add(
+                email,
+                company,
+                status,
+                createButtonsLayout());
+    }
+
+    private HorizontalLayout createButtonsLayout() {
+
+        return new HorizontalLayout();
+    }
 }
--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,7 +1,9 @@
 package com.example.application.views.list;
 
 
+import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Contact;
+import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -13,6 +15,10 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.awt.*;
+import java.util.Collection;
+import java.util.Collections;
+
 
 @PageTitle("Contact | Vaadin CRM")
 @Route(value = "")
@@ -20,17 +26,32 @@ public class ListView extends VerticalLayout {
 
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
+    ContactForm form;
 
     public ListView() {
         addClassName("list-view");
         setSizeFull();
         configureGrid();
+        configureForm();
 
         add(
                 getToolbar(),
-                grid
+                getContent()
         );
 
+    }
+
+    private void configureForm(){
+        form = new ContactForm(Collections.emptyList(), Collections.emptyList());
+        form.setWidth("25em");
+    }
+    private Component getContent(){
+       HorizontalLayout content = new HorizontalLayout(grid, form);
+       content.setFlexGrow(2, grid);
+       content.setFlexGrow(1, form);
+       content.addClassName("content");
+       content.setSizeFull();
+        return content;
     }
 
     private Component getToolbar() {
