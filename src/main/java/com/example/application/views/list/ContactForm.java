c53aaaa22d4345544a7c614817cc3e24f50d2f06
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-25 18:44:00 -0600

    added in binder for contact form

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -2,19 +2,23 @@ package com.example.application.views.list;
 
 import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Status;
-import com.vaadin.flow.component.Key;
+import com.example.application.data.entity.Contact;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+
 
 import java.util.List;
 
 
 public class ContactForm extends FormLayout {
+
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
     TextField firstName = new TextField("First Name");
     TextField lastName = new TextField("Last Name");
     EmailField email = new EmailField("Email");
@@ -27,6 +31,7 @@ public class ContactForm extends FormLayout {
 
     public ContactForm(List<Company> companies, List<Status> statuses) {
         addClassName("contact-form");
+        binder.bindInstanceFields(this);
         company.setItems(companies);
         company.setItemLabelGenerator(Company::getName);
         status.setItems(statuses);
